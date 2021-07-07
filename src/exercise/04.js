f73fdee12cda0f687ea0117ce5cd49a4fefb42b2
Max MacMillan <max@alley.co>
2021-07-07 16:22:30 -0700

    Working game using derived state from utility functions

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,41 +1,40 @@
 // useState: tic tac toe
 // http://localhost:3000/isolated/exercise/04.js
 
-import * as React from 'react'
+import React, { useState } from 'react'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
+  // Derived state variables (derived from other state)
+  const nextValue = calculateNextValue(squares);
+  const winner = calculateWinner(squares);
+  const status = calculateStatus(winner, squares, nextValue);
 
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
+    // Note to self:
+    // This was hard for me to grok - we are checking if the squares state var
+    // has a value at the index of 'square' (the index of the clicked square);
+    if (winner || squares[square] ) {
+      return;
+    }
+
+    // Don't mutate state directly, make a copy.
+    const currentSquares = [...squares];
+
+    // Use the array index to set the next value on our clicked square.
+    currentSquares[square] = nextValue;
+
+    // Update the whole piece of state as usual.
+    setSquares(currentSquares);
   }
 
   function restart() {
     // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
+    setSquares(Array(9).fill(null));
   }
 
   function renderSquare(i) {
@@ -49,7 +48,7 @@ function Board() {
   return (
     <div>
       {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
