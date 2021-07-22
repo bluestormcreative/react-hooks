d76006c2dd9086d5c30ab8c8bdfdc3c63e883ddf
Max MacMillan <max@alley.co>
2021-07-22 10:24:13 -0700

    Set up useEffect base, return early if name is falsy, fetch if truthy

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,7 +6,7 @@ import React, {useState, useEffect} from 'react'
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {PokemonForm, fetchPokemon} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
@@ -24,6 +24,16 @@ function PokemonInfo({pokemonName}) {
   //   1. no pokemonName: 'Submit a pokemon'
   //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
   //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+
+  useEffect(() => {
+    if (pokemonName === '') {
+      setPokemon(null);
+      return;
+    }
+
+    fetchPokemon(pokemonName);
+
+  }, [pokemonName, pokemon]);
 
   // 💣 remove this
   return 'TODO'
