c21221651168dd043fe6135351698a766d734c30
Max MacMillan <max@alley.co>
2021-07-29 09:49:11 -0700

    Add status state and switch statement to return based on this

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,10 +2,7 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import React, {useState, useEffect} from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
+
 import {
   PokemonForm,
   PokemonInfoFallback,
@@ -23,50 +20,59 @@ function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
   const [pokemon, setPokemon] = useState(null);
   const [error, setError] = useState(null);
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => { /* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  const [status, setStatus] = useState('idle');
 
   useEffect(() => {
+    setStatus('idle')
     if (!pokemonName) {
       return;
     }
     setPokemon(null);
 
+    setStatus('pending');
     fetchPokemon(pokemonName)
     .then(
       pokemonData => {
         setPokemon(pokemonData);
+        setStatus('resolved');
       }
     ).catch(err => {
       setError(err);
+      setStatus('rejected');
     });
   }, [pokemonName]);
 
-  if (error !== null) {
-    return <PokeError error={error} />
-  }
+  // if (error !== null) {
+  //   return <PokeError error={error} />
+  // }
 
-  if (! pokemonName) {
-    return 'Submit a pokemon!';
-  }
+  // if (! pokemonName) {
+  //   return 'Submit a pokemon!';
+  // }
 
-  if (pokemonName && pokemon === null) {
-    return <PokemonInfoFallback name={pokemonName} />
-  }
+  // if (pokemonName && pokemon === null) {
+  //   return <PokemonInfoFallback name={pokemonName} />
+  // }
 
-  if (pokemon !== null) {
-    return <PokemonDataView pokemon={pokemon} />
+  // if (pokemon !== null) {
+  //   return <PokemonDataView pokemon={pokemon} />
+  // }
+
+  switch(status) {
+    case 'idle' :
+      return 'Submit a pokemon!';
+
+    case 'rejected' :
+      return <PokeError error={error} />;
+
+    case 'pending' :
+      return <PokemonInfoFallback name={pokemonName} />;
+
+    case 'resolved' :
+      return <PokemonDataView pokemon={pokemon} />;
+
+    default :
+      return 'Submit a pokemon!';
   }
 }
 
